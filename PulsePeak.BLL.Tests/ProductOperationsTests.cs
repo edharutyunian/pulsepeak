7dfb107f1c5caba14e15ffe3ef91713a9be0a7de
tatevtshagharyan <tatev.tshagharyan@gmail.com>
2024-05-12 17:23:11 +0400

    DeactivateProduct tests are finalized

--- PulsePeak.BLL.Tests/ProductOperationsTests.cs
+++ PulsePeak.BLL.Tests/ProductOperationsTests.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using PulsePeak.BLL.Operations;
 using PulsePeak.Core.BLLOperationContracts;
+using PulsePeak.Core.Entities.Products;
 using PulsePeak.Core.Entities.Users;
 using PulsePeak.Core.Exceptions;
+using PulsePeak.Core.RepositoryContracts.EntityRepositoryContracts;
 using PulsePeak.Core.RepositoryContracts.RepositoryAbstraction;
 using PulsePeak.Core.ViewModels;
 using PulsePeak.Core.ViewModels.UserViewModels.MerchantViewModels;
@@ -126,7 +128,7 @@ namespace PulsePeak.BLL.Tests
             var productOperations = new ProductOperations(loggerMock.Object, repositoryHandlerMock.Object, mapperMock.Object);
 
             // Act & Assert
-            await Assert.ThrowsAsync<RegistrationException>(() => productOperations.AddProduct(merchantId,productModel));
+            await Assert.ThrowsAsync<RegistrationException>(() => productOperations.AddProduct(merchantId, productModel));
         }
 
         [Fact]
@@ -135,7 +137,7 @@ namespace PulsePeak.BLL.Tests
             // Arrange
 
 
-            var productModel = new ProductModel() 
+            var productModel = new ProductModel()
             {
                 CreatedOn = DateTime.UtcNow,
                 Title = "Top",
@@ -160,6 +162,75 @@ namespace PulsePeak.BLL.Tests
             await Assert.ThrowsAsync<EntityNotFoundException>(() => productOperations.AddProduct(999 /* Provide non-existing merchant ID */, productModel));
         }
 
-        
+        [Fact]
+        public async Task DeactivateProduct_ProductExists_DeactivatesProduct()
+        {
+            // Arrange
+            var productId = 1;
+            var product = new ProductBaseEntity
+            {
+                Id = productId,
+                Active = true,
+                Merchant = null,
+                Title = "Title",
+            };
+
+            repositoryHandlerMock.Setup(rh => rh.ProductRepository.GetSingleAsync(It.IsAny<Expression<Func<ProductBaseEntity, bool>>>()))
+                .ReturnsAsync(product);
+            repositoryHandlerMock.Setup(rh => rh.ProductRepository.Update(It.IsAny<ProductBaseEntity>()))
+                .Returns(true);
+            var productOperations = new ProductOperations(loggerMock.Object, repositoryHandlerMock.Object, mapperMock.Object);
+
+            // Act
+            await productOperations.DeactivateProduct(productId);
+
+            // Assert
+            Assert.False(product.Active);
+
+        }
+
+        [Fact]
+        public async Task DeactivateProduct_ProductNotFound_ThrowsEntityNotFoundException()
+        {
+            // Arrange
+
+            var productId = 999;
+
+            repositoryHandlerMock.Setup(rh => rh.ProductRepository.GetSingleAsync(It.IsAny<Expression<Func<ProductBaseEntity, bool>>>()))
+                .ReturnsAsync((ProductBaseEntity)null);
+
+            var productOperations = new ProductOperations(loggerMock.Object, repositoryHandlerMock.Object, mapperMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => productOperations.DeactivateProduct(productId));
+        }
+
+        [Fact]
+        public async Task DeactivateProduct_UpdateFails_ThrowsDbContextException()
+        {
+            // Arrange
+            var productId = 1;
+            var product = new ProductBaseEntity
+            {
+                Id = productId,
+                Active = true,
+                Merchant = null,
+                Title = "Title",
+            };
+
+            repositoryHandlerMock.Setup(rh => rh.ProductRepository.GetSingleAsync(It.IsAny<Expression<Func<ProductBaseEntity, bool>>>()))
+                .ReturnsAsync(product);
+
+            repositoryHandlerMock.Setup(rh => rh.ProductRepository.Update(It.IsAny<ProductBaseEntity>()))
+                .Returns(false);
+
+            var productOperations = new ProductOperations(loggerMock.Object, repositoryHandlerMock.Object, mapperMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<DbContextException>(() => productOperations.DeactivateProduct(productId));
+        }
+
+
+
     }
 }
